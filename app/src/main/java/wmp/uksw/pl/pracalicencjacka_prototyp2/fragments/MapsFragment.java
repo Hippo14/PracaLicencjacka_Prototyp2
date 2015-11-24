e022c2f8df5e29a92f44180f9f296a22f864398e
Hippo14 <hippo14@gmail.com>
2015-11-24 12:33:24 +0100

    Added onResume, onPause etc

--- app/src/main/java/wmp/uksw/pl/pracalicencjacka_prototyp2/fragments/MapsFragment.java
+++ app/src/main/java/wmp/uksw/pl/pracalicencjacka_prototyp2/fragments/MapsFragment.java
@@ -1,10 +1,12 @@
 package wmp.uksw.pl.pracalicencjacka_prototyp2.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
@@ -33,6 +35,26 @@ public class MapsFragment extends SupportMapFragment {
         initMap();
 
         return v;
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        //Toast.makeText(getActivity(), "onDestroy();", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onPause() {
+        super.onPause();
+        //Toast.makeText(getActivity(), "onPause();", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onResume() {
+        super.onResume();
+        //Toast.makeText(getActivity(), "onResume();", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        //Toast.makeText(getActivity(), "onDestroyView();", Toast.LENGTH_SHORT).show();
     }
 
     private void initMap() {
